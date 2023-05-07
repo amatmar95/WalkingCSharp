6d41533b55245832aa136a3123b17a0a98314872
amatmar95 <amatmar95@gmail.com>
2023-05-07 18:18:38 +0200

    Unidad 2 ejercicio 2 completado

--- Final/Program.cs
+++ Final/Program.cs
@@ -6,17 +6,12 @@ class ProgramaMejorado{
         
         int examAssignments = 5;
 
+        string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
+
         int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
         int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
         int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
         int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
-        int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
-        int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
-        int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
-        int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };    
-
-
-        string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
 
         int[] studentScores = new int[10];
 
@@ -24,9 +19,9 @@ class ProgramaMejorado{
 
 
         Clear();
-        WriteLine("Student\t\tGrade\n");
+        WriteLine("Student\t\tExam Score\tOverall Grade\tExtra Credit\n");
 
-
+        
         foreach (string name in studentNames)
         {
             string currentStudent = name;
@@ -42,25 +37,16 @@ class ProgramaMejorado{
 
             else if (currentStudent == "Logan")
                 studentScores = loganScores;
-            
-            else if (currentStudent == "Becky")
-                studentScores = beckyScores;
 
-            else if (currentStudent == "Chris")
-                studentScores = chrisScores;
-
-            else if (currentStudent == "Eric")
-                studentScores = ericScores;
-
-            else if (currentStudent == "Gregor")
-                studentScores = gregorScores;
-                
-            else
-                continue;
-
-            int sumAssignmentScores = 0;
-            decimal currentStudentGrade = 0;
             int gradedAssignments = 0;
+            int gradedExtraCreditAssignments = 0;
+
+            int sumExamScores = 0;
+            int sumExtraCreditScores = 0;
+
+            decimal currentStudentGrade = 0;
+            decimal currentStudentExamScore = 0;
+            decimal currentStudentExtraCreditScore = 0;
 
             
             foreach (int score in studentScores)
@@ -68,46 +54,67 @@ class ProgramaMejorado{
                 gradedAssignments += 1;
 
                 if (gradedAssignments <= examAssignments)
-                    sumAssignmentScores += score;
+                {
+                    sumExamScores = sumExamScores + score;
+                }
+
                 else
-                    sumAssignmentScores += score / 10;
+                {
+                    gradedExtraCreditAssignments += 1;
+                    sumExtraCreditScores += score;
+                }
             }
 
-            currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+            currentStudentExamScore = (decimal)(sumExamScores) / examAssignments;
+            currentStudentExtraCreditScore = (decimal)(sumExtraCreditScores) / gradedExtraCreditAssignments;
+
+            currentStudentGrade = (decimal)((decimal)sumExamScores + ((decimal)sumExtraCreditScores / 10)) / examAssignments;
 
             if (currentStudentGrade >= 97)
                 currentStudentLetterGrade = "A+";
+
             else if (currentStudentGrade >= 93)
                 currentStudentLetterGrade = "A";
+
             else if (currentStudentGrade >= 90)
                 currentStudentLetterGrade = "A-";
+
             else if (currentStudentGrade >= 87)
                 currentStudentLetterGrade = "B+";
+
             else if (currentStudentGrade >= 83)
                 currentStudentLetterGrade = "B";
+
             else if (currentStudentGrade >= 80)
                 currentStudentLetterGrade = "B-";
+
             else if (currentStudentGrade >= 77)
                 currentStudentLetterGrade = "C+";
+
             else if (currentStudentGrade >= 73)
                 currentStudentLetterGrade = "C";
+
             else if (currentStudentGrade >= 70)
                 currentStudentLetterGrade = "C-";
+
             else if (currentStudentGrade >= 67)
                 currentStudentLetterGrade = "D+";
+
             else if (currentStudentGrade >= 63)
                 currentStudentLetterGrade = "D";
+
             else if (currentStudentGrade >= 60)
                 currentStudentLetterGrade = "D-";
+
             else
                 currentStudentLetterGrade = "F";
 
-        
-            
-            WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+
+
+            WriteLine($"{currentStudent}\t\t{currentStudentExamScore}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}\t{currentStudentExtraCreditScore} ({(((decimal)sumExtraCreditScores / 10) / examAssignments)} pts)");
         }
 
-
+        
         WriteLine("\n\rPress the Enter key to continue");
         ReadLine();
 
--- Inicial/Program.cs
+++ Inicial/Program.cs
@@ -1,58 +1,101 @@
 ﻿using static System.Console;
 
 
+int examAssignments = 5;
 
-int currentAssignments = 5;
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan" };
 
-int sophia1 = 90;
-int sophia2 = 86;
-int sophia3 = 87;
-int sophia4 = 98;
-int sophia5 = 100;
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
 
-int andrew1 = 92;
-int andrew2 = 89;
-int andrew3 = 81;
-int andrew4 = 96;
-int andrew5 = 90;
+int[] studentScores = new int[10];
 
-int emma1 = 90;
-int emma2 = 85;
-int emma3 = 87;
-int emma4 = 98;
-int emma5 = 68;
+string currentStudentLetterGrade = "";
 
-int logan1 = 90;
-int logan2 = 95;
-int logan3 = 87;
-int logan4 = 88;
-int logan5 = 96;
 
-int sophiaSum = 0;
-int andrewSum = 0;
-int emmaSum = 0;
-int loganSum = 0;
+Clear();
+WriteLine("Student\t\tGrade\tLetter Grade\n");
 
-decimal sophiaScore;
-decimal andrewScore;
-decimal emmaScore;
-decimal loganScore;
 
-sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
-andrewSum = andrew1 + andrew2 + andrew3 + andrew4 + andrew5;
-emmaSum = emma1 + emma2 + emma3 + emma4 + emma5;
-loganSum = logan1 + logan2 + logan3 + logan4 + logan5;
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
 
-sophiaScore = (decimal)sophiaSum / currentAssignments;
-andrewScore = (decimal)andrewSum / currentAssignments;
-emmaScore = (decimal)emmaSum / currentAssignments;
-loganScore = (decimal)loganSum / currentAssignments;
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
 
-WriteLine("Student\t\tGrade\n");
-WriteLine("Sophia:\t\t" + sophiaScore + "\tA-");
-WriteLine("Andrew:\t\t" + andrewScore + "\tB+");
-WriteLine("Emma:\t\t" + emmaScore + "\tB");
-WriteLine("Logan:\t\t" + loganScore + "\tA-");
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
 
-WriteLine("Press the Enter key to continue");
-ReadLine();
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+
+    int sumAssignmentScores = 0;
+
+    decimal currentStudentGrade = 0;
+
+    int gradedAssignments = 0;
+
+    
+    foreach (int score in studentScores)
+    {
+        gradedAssignments += 1;
+
+        if (gradedAssignments <= examAssignments)
+            sumAssignmentScores += score;
+
+        else
+            sumAssignmentScores += score / 10;
+    }
+
+    currentStudentGrade = (decimal)(sumAssignmentScores) / examAssignments;
+
+    if (currentStudentGrade >= 97)
+        currentStudentLetterGrade = "A+";
+
+    else if (currentStudentGrade >= 93)
+        currentStudentLetterGrade = "A";
+
+    else if (currentStudentGrade >= 90)
+        currentStudentLetterGrade = "A-";
+
+    else if (currentStudentGrade >= 87)
+        currentStudentLetterGrade = "B+";
+
+    else if (currentStudentGrade >= 83)
+        currentStudentLetterGrade = "B";
+
+    else if (currentStudentGrade >= 80)
+        currentStudentLetterGrade = "B-";
+
+    else if (currentStudentGrade >= 77)
+        currentStudentLetterGrade = "C+";
+
+    else if (currentStudentGrade >= 73)
+        currentStudentLetterGrade = "C";
+
+    else if (currentStudentGrade >= 70)
+        currentStudentLetterGrade = "C-";
+
+    else if (currentStudentGrade >= 67)
+        currentStudentLetterGrade = "D+";
+
+    else if (currentStudentGrade >= 63)
+        currentStudentLetterGrade = "D";
+
+    else if (currentStudentGrade >= 60)
+        currentStudentLetterGrade = "D-";
+
+    else
+        currentStudentLetterGrade = "F";
+
+    WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currentStudentLetterGrade}");
+}
+
+WriteLine("\n\rPress the Enter key to continue");
+Console.ReadLine();
